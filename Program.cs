e7e8a73ebba9cbf08ea38246e3e89491167cfc83
tian-dutoit <tiandutoit.nz@gmail.com>
2018-04-29 13:51:01 +1200

    loops exercise

--- Program.cs
+++ Program.cs
@@ -70,6 +70,88 @@ namespace C_sharp_begginer
                 } 
             }
         }
+
+        public class Loops {
+            //  Write a program to count how many numbers between 1 and 100 are divisible by 3 with no remainder. Display the count on the console.
+            public static void ExcerciseOne(){
+                var count = 0;
+                for (int i = 0; i < 100; i++)
+                {
+                    if (i%3 == 0)
+                    {
+                        count ++;
+                    }
+                }
+                System.Console.WriteLine(count);
+            }           
+            
+            // 2- Write a program and continuously ask the user to enter a number or "ok" to exit. Calculate the sum of all the previously entered numbers and display it on the console.
+            public static void ExcerciseTwo(){
+                var sum = 0;
+                while (true)
+                {
+                    System.Console.WriteLine("Please enter a number or type 'ok' to exit");
+                    var input = Console.ReadLine();
+                    if(input == "ok") {
+                        break;
+                    } else
+                    {
+                        sum += Convert.ToInt32(input);   
+                    }                    
+                }
+                System.Console.WriteLine(sum);
+            } 
+
+
+            // 3- Write a program and ask the user to enter a number. Compute the factorial of the number and print it on the console. For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 and display it as 5! = 120.
+            public static void ExcerciseThree(){
+                var sum = 1;
+                System.Console.WriteLine("Please enter a number");
+                var number = Convert.ToInt32(Console.ReadLine());
+                for (int i = number; i > 0; i--)
+                {
+                    sum *= i;
+                }
+                System.Console.WriteLine("{}! = {}", number, sum);
+            }
+
+
+            // 4- Write a program that picks a random number between 1 and 10. Give the user 4 chances to guess the number. If the user guesses the number, display “You won"; otherwise, display “You lost". (To make sure the program is behaving correctly, you can display the secret number on the console first.)
+            public static void ExcerciseFour(){
+                var secret = new Random().Next(1, 10);
+
+                for (int i = 0; i < 4; i++)
+                {
+                    System.Console.WriteLine("Guess the secret number between one and ten");
+                    var guess = Convert.ToInt32(Console.ReadLine());
+                    if (guess == secret)
+                    {
+                        System.Console.WriteLine("You won");
+                        return; //get out of loop if they are correct
+                    }
+                }
+                System.Console.WriteLine("You lost");
+            }
+
+
+            // 5- Write a program and ask the user to enter a series of numbers separated by comma. Find the maximum of the numbers and display it on the console. For example, if the user enters “5, 3, 8, 1, 4", the program should display 8.
+            public static void ExcerciseFive(){
+                System.Console.WriteLine("Please enter a series of numbers separated by commas");
+                var input = Console.ReadLine();
+                var numbers = input.Split(',');
+                var maximum = 0;
+                foreach (var stringNumber in numbers)
+                {
+                    var number = Convert.ToInt32(stringNumber);
+                    if (number > maximum)
+                    {
+                        maximum = number;
+                    }
+                }
+                System.Console.WriteLine(maximum);
+            }
+
+        }
         static void Main(string[] args)
         {
             Conditionals.ExcerciseOne();
