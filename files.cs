b0b6b6d48389fc2c1cd003752604dcd7d16a3988
tian-dutoit <tiandutoit.nz@gmail.com>
2018-05-01 13:23:13 +1200

    files demo

--- files.cs
+++ files.cs
@@ -1,9 +1,28 @@
 using System;
-
+using System.IO;
 
 namespace C_sharp_begginer
 {
-  public class files {
+  public class Files {
+    static void fileDemo () {
+      File.Copy("c:\\temp\\tocopy.jpg", "c:\\temp\\myflie.jpg");
+      File.Delete("path");
+      if (File.Exists("path"))
+      {
+          //do something
+      }
+      var content = File.ReadAllText("path");
+    }
 
+    static void fileInfoDemo () {
+      var fileInfo = new FileInfo("path");
+      fileInfo.CopyTo("...");
+      fileInfo.Delete();
+    }
+
+      static void directoryDemo () {
+        Directory.CreateDirectory("path");
+        Directory.GetFiles("path", "search patter like .jpg", SearchOption.AllDirectories);
+    }
   }
 }
